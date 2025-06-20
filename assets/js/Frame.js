868be8fa0f07178a92d68d463c80f0832b9d4e98
LEH <woonari@gmail.com>
2025-06-20 16:39:37 +0900

    음...

--- assets/js/Frame.js
+++ assets/js/Frame.js
@@ -1,11 +1,7 @@
 export const Frame = () => {
   const currentState = sessionStorage.getItem("src");
   const frame = document.querySelector("iframe");
-  const basePath = "/js-homework/";
-  const initial = "00Basic/01-start.html";
+  const initial = "js-homework/00Basic/01-start.html";
 
-  const fullPath = basePath + (currentState || initial);
-
-  //frame.setAttribute("src", currentState || initial);
-  frame.setAttribute("src", fullPath);
+  frame.setAttribute("src", currentState || initial);
 };
