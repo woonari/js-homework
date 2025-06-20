f7283c2cb9fd07a09f7756868934411339d49881
LEH <woonari@gmail.com>
2025-06-20 16:42:24 +0900

    수정

--- assets/js/Frame.js
+++ assets/js/Frame.js
@@ -1,7 +1,20 @@
+// export const Frame = () => {
+//   const currentState = sessionStorage.getItem("src");
+//   const frame = document.querySelector("iframe");
+//   const initial = "js-homework/00Basic/01-start.html";
+
+//   frame.setAttribute("src", currentState || initial);
+// };
+
+
 export const Frame = () => {
   const currentState = sessionStorage.getItem("src");
   const frame = document.querySelector("iframe");
-  const initial = "js-homework/00Basic/01-start.html";
+  const basePath = "/js-homework/";
+  const initial = "00Basic/01-start.html"; // 상대 경로로 바꿨음
 
-  frame.setAttribute("src", currentState || initial);
+  // 주소 앞에 항상 basePath를 붙여줌
+  const fullPath = basePath + (currentState || initial);
+
+  frame.setAttribute("src", fullPath);
 };
