1380204b1aa12817a3ecf52ba5024aa59521e0b8
LEH <woonari@gmail.com>
2025-06-20 16:37:19 +0900

    수정

--- assets/js/Frame.js
+++ assets/js/Frame.js
@@ -1,7 +1,11 @@
 export const Frame = () => {
   const currentState = sessionStorage.getItem("src");
   const frame = document.querySelector("iframe");
-  const initial = "https://woonari.github.io/js-homework/00Basic/01-start.html";
+  const basePath = "/js-homework/";
+  const initial = "00Basic/01-start.html";
 
-  frame.setAttribute("src", currentState || initial);
+  const fullPath = basePath + (currentState || initial);
+
+  //frame.setAttribute("src", currentState || initial);
+  frame.setAttribute("src", fullPath);
 };
